05c990675fa51ab416f0c2997302d09e0d7ed129
Edward <duongedward44@gmail.com>
2022-06-21 14:17:32 -0500

    same where we left off

--- script.js
+++ script.js
@@ -3,14 +3,16 @@ const ul = document.getElementById('the-ul')
 const newLi = document.createElement('li')
 ul.appendChild(newLi)
 
-const table = document.getElementsByTagName('table')[0]
+const table = document.getElementsByTagName('table')
 
-function makeRow(){
+function makeRow () {
     const row = document.createElement('tr')
-    for ( let i = 0; i < 20; i++){
-        const td =document.createElement('td')
+    for (let i = 0; i < 20; i++) {
+        const td = document.createElement('td')
         row.appendChild(td)
     }
     table.appendChild(row)
 }
-makeRow(2)
+
+makeRow()
+makeRow()
